89ae2d5e9bf331bdfaf5fa262e68814d15f81c95
Artem Semirenko <artemiyka95@gmail.com>
2024-05-22 14:21:20 +0300

    Update main.dart
    change api key to env var

--- lib/flavor/main.dart
+++ lib/flavor/main.dart
@@ -5,8 +5,7 @@ void main() {
   setupApp(
     Environment(
       apiUrl: 'https://api.openweathermap.org/data/2.5/',
-      // this key is free no sense to secret it
-      apiKey: '5dcc4e458c9c37f0783f6a0b9d296e71',
+      apiKey: Platform.environment['WHEATHER_API_KEY'] ?? '',
     ),
   );
 }
